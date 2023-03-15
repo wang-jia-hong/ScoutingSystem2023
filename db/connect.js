9e5c6c4cd722ede98bdc3554fe75bedbe05f9237
wang-jia-hong <h010046@nehs.tc.edu.tw>
2023-03-15 23:01:48 +0800

    介面改善、改善平板介面支援度

--- db/connect.js
+++ db/connect.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const dbCilent = new MongoClient('mongodb://' + process.env.dbUser + ':' + encodeURIComponent(process.env.dbPassword) + '@' + process.env.dbURI);
 
-
 const connectDB = async () => {
 	await dbCilent.connect();
 };
--- result/public/js/footer.js
+++ result/public/js/footer.js
@@ -1,4 +1,6 @@
-const showToolbar = () => {
-    document.getElementById('toolbar-div').classList.toggle('toolbar-div-show');
-    document.getElementById('f-logo-div').classList.toggle('f-logo-div-noShadow');
+const showToolbar = (byHover) => {
+    if( ( !byHover && navigator.maxTouchPoints > 0) || ( byHover && navigator.maxTouchPoints <= 0)) {
+        document.getElementById('toolbar-div').classList.toggle('toolbar-div-show');
+        document.getElementById('f-logo-div').classList.toggle('f-logo-div-noShadow');
+    }
 };
