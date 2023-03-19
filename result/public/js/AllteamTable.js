2340652e507c935e550d6be4c8e7b1c58d10a0de
wang-jia-hong <h010046@nehs.tc.edu.tw>
2023-03-19 21:43:22 +0800

    bug修復

--- result/public/js/AllteamTable.js
+++
@@ -1,8 +0,0 @@
-const charts = document.querySelectorAll('.chart');
-['mouseover', 'mouseout'].forEach(event => {
-    charts.forEach( chart => {
-        chart.addEventListener(event, () => {
-            chart.classList.toggle('chart-withBefore');
-        });
-    });
-});
