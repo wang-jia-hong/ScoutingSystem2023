417e7774bfba0085e328b34e3dff59daf79fe48e
wang-jia-hong <h010046@nehs.tc.edu.tw>
2023-03-19 21:15:45 +0800

    查閱端因應新規則更改

--- db/connect.js
+++ db/connect.js
@@ -2,8 +2,8 @@ const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
-const dbCilent = new MongoClient('mongodb://' + process.env.dbUser + ':' + encodeURIComponent(process.env.dbPassword) + '@' + process.env.dbURI);
-
+// const dbCilent = new MongoClient('mongodb://' + process.env.dbUser + ':' + encodeURIComponent(process.env.dbPassword) + '@' + process.env.dbURI);
+const dbCilent = new MongoClient('mongodb+srv://test:VJiZkyFokvvsedqt@investigationsystem.rj4w2sp.mongodb.net/?retryWrites=true&w=majority');
 
 const connectDB = async () => {
 	await dbCilent.connect();
