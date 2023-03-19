4be6d322254ff3749d2076d00cd5043241c4367f
wang-jia-hong <h010046@nehs.tc.edu.tw>
2023-03-19 22:40:02 +0800

    bug修復

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
-const dbCilent = new MongoClient('mongodb://' + process.env.dbUser + ':' + encodeURIComponent(process.env.dbPassword) + '@' + process.env.dbURI + '/?retryWrites=true&w=majority');
+const dbCilent = new MongoClient('mongodb://' + process.env.dbUser + ':' + encodeURIComponent(process.env.dbPassword) + '@' + process.env.dbURI + '/?retryWrites=true');
 
 const connectDB = async () => {
 	await dbCilent.connect();
